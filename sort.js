a8e6144903bb8f785938764cc60d547df296eadd
Amy Lee <amyleeannebennett@gmail.com>
2016-03-09 14:26:48 -0600

    adding insertion and selection sort

--- sort.js
+++ sort.js
@@ -1,7 +1,8 @@
 'use strict'
 
-// const arr = [6,5,3,1,8,7,2,4]
-const arr = [9,6,5,3,1,8,7,2,4]
+const chalk = require('chalk')
+
+const arr = [6,5,3,1,8,7,2,4]
 
 console.log('start', arr)
 
@@ -13,18 +14,49 @@ function swap (array, index1, index2) {
 }
 
 function bubbleSort (array) {
-
-  for (let j = 1; i < array.length; j++) {
+  for (let j = 1; j < array.length; j++) {
     for (let i = 0; i < array.length - j; i++) {
       if (array[i] > array[i + 1]) {
         swap(array, i, i + 1)
-        console.log(array)
+        console.log(chalk.red(array))
       }
+      console.log(array)
     }
   }
-
 
   return array
 }
 
 console.log('bubble', bubbleSort(arr))
+
+function insertionSort (array) {
+  for (let i=1; i < array.length; i++) {
+    for (let j=i; j > 0; j--){
+      if (array[j-1] > array[j]) {
+        swap(array, j-1, j)
+        console.log(util.inspect(array));
+      }else {
+        console.log(util.inspect(array));
+      }
+    }
+  } return array
+}
+
+function selectionSort (array){
+  for (let i =0; i < array.length - 1; i++){
+    let minimumIndex = i;
+
+    for (let j= i + 1; j < array.length; j++){
+      if (array[minimumIndex] > array[j]){
+        minimumIndex = j
+      }
+    }
+    if (minimumIndex !== i){
+      swap(array, minimumIndex, i)
+      console.log(util.inspect(array));
+    } else {
+      console.log(util.inspect(array));
+    }
+  }
+  return array
+}
